f4ebea90c5b6cd2ce3968e07bc075e86f60073ad
Besar Kutleshi <besar.kutleshi0@gmail.com>
2020-11-18 22:56:32 +0100

    employee managment added

--- src/collaboration/collaboration.js
+++ src/collaboration/collaboration.js
@@ -53,5 +53,65 @@ class Collaboration{
             return false;
         }
     }
+
+    async insertEmployee(obj){
+        try {
+            let response = await axios.post(Helper.url + 'insertEmployee',obj,Helper.config);
+            return Helper.response(response);
+        } catch (error) {
+            ErrorAlert(error.message);
+            return false;
+        }
+    }
+
+    async deleteEmployee(id){
+        try {
+            let response = await axios.get(Helper.url + `deleteEmployee/${id}`);
+            return Helper.response(response);
+        } catch (error) {
+            ErrorAlert(error.message);
+            return false;
+        }
+    }
+
+    async updateEmployee(obj){
+        try {
+            let response = await axios.post(Helper.url + 'updateEmployee',obj,Helper.config);
+            return Helper.response(response);
+        } catch (error) {
+            ErrorAlert(error.message);
+            return false;
+        }
+    }
+
+    async getEmployees(){
+        try {
+            let response = await axios.get(Helper.url + 'getEmployees',Helper.config);
+            return response.data;
+        } catch (error) {
+            ErrorAlert(error.message);
+            return false;
+        }
+    }
+
+    async getEmployeeById(id){
+        try {
+            let response = await axios.get(Helper.url + `getEmployee/${id}`);
+            return response.data;
+        } catch (error) {
+            ErrorAlert(error.message);
+            return false;
+        }
+    }
+    
+    async getEmployeeByName(name){
+        try {
+            let response = await axios.get(Helper.url + `getEmployee/${name}`);
+            return response.data;
+        } catch (error) {
+            ErrorAlert(error.message);
+            return false;
+        }
+    }
 }
 export default new Collaboration();
